1159277e181098830289d553abe0027c065da0fb
notSaqlain <iamnotsaqlain@gmail.com>
2024-11-14 09:26:56 +0100

    refactor Main.java to move Coppia instances into main method

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 
-import java.util.*;
-import javax.sound.sampled.SourceDataLine;
 
 public class Main {
-    Coppia c1 = new Coppia<String, Integer>("Saqlain", 1);
-    Coppia c2 = new Coppia<Double, Long>(3.14, 500l);
+    public static void main(String[] args) {
+        Coppia c1 = new Coppia<String, Integer>("Saqlain", 1);
+        Coppia c2 = new Coppia<Double, Long>(3.14, 500l);
 
-    System.out.println(c1);
-    System.out.println(c2);
+        System.out.println(c1);
+        System.out.println(c2);
+    }
 
 }
