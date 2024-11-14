f62ecef620152ebeaa8b796e82b3045d55c5e1a2
notSaqlain <iamnotsaqlain@gmail.com>
2024-11-14 09:33:01 +0100

    add ArrayList example and print statements in Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 
+import java.util.ArrayList;
+
+
 
 public class Main {
     public static void main(String[] args) {
         Coppia c1 = new Coppia<String, Integer>("Saqlain", 1);
         Coppia c2 = new Coppia<Double, Long>(3.14, 500l);
 
+        ArrayList<Double> v = new ArrayList<>();
+        v.add(3.14);
+        v.add((double)43);
+
+        System.out.println(v);
         System.out.println(c1);
         System.out.println(c2);
     }
